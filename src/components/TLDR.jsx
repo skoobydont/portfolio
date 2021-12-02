583032e4d97550b83b44a009b87471d90663c165
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-12-02 16:09:24 -0500

    init tldr component

---
+++ src/components/TLDR.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+/**
+ * TLDR Component. Basically the About Me portion of portfolio
+ * @returns {Component}
+ */
+const TLDR = () => {
+  return (
+    <div id="tldr">
+      <code>
+        TLDR: not your average tech enthusiast
+      </code>
+      {/* TODO: more thorough about me stuff after the tldr */}
+    </div>
+  );
+};
+
+export default TLDR;
