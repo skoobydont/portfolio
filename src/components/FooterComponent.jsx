4cbdcd7749d2b631a56372679949350ac09574ab
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-12-06 10:23:37 -0500

    added linkedin and github links to bottom nav

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,33 +4,65 @@ import { useHistory } from 'react-router';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import { makeStyles } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 // Icons
 import HomeIcon from '@material-ui/icons/Home';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'start',
+    justifyContent: 'center',
   },
   icon: {
     maxWidth: theme.spacing(5),
-    marginBottom: theme.spacing(1),
+    // marginBottom: theme.spacing(1),
     marginLeft: theme.spacing(-1),
+    '& :hover': {
+      color: "#FFF",
+    },
   },
 }));
 
 const Footer = () => {
   const classes = useStyles();
   const history = useHistory();
-  const handleHomeRedirect = () => history?.push('/portfolio');
+  const handleHomeRedirect = () => history?.push(process.env.REACT_APP_HOME_URL);
+  const handleOpenTab = (link) => {
+    const win = window.open(link, "_blank");
+    win.focus();
+    return null;
+  }
   return (
     <BottomNavigation className={classes.root}>
       <BottomNavigationAction
-        icon={<HomeIcon />}
+        icon={(
+          <Tooltip title="Home">
+            <HomeIcon />
+          </Tooltip>
+        )}
         className={classes.icon}
         onClick={handleHomeRedirect}
+      />
+      <BottomNavigationAction
+        icon={(
+          <Tooltip title="LinkedIn">
+            <LinkedInIcon />          
+          </Tooltip>
+        )}
+        className={classes.icon}
+        onClick={() => handleOpenTab(process.env.REACT_APP_LINKED_IN_URL)}
+      />
+      <BottomNavigationAction
+        icon={(
+          <Tooltip title="GitHub">
+            <GitHubIcon />
+          </Tooltip>
+        )}
+        className={classes.icon}
+        onClick={() => handleOpenTab(process.env.REACT_APP_GITHUB_URL)}
       />
     </BottomNavigation>
   );
