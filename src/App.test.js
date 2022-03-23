0962d77f665ac385370ed5b9be4c963e0f9a5b9b
skoob <ajskubak@gmail.com>
2022-03-22 21:54:00 -0400

    updated app.js test file. may need to go and do additional tests for each component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('main app file tests', () => {
+  // Reusable logic for each test case
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('renders nav component & button', () => {
+    // Ensure nav bar component renders
+    const navComponent = screen.getByTestId(/nav/i);
+    expect(navComponent).toBeInTheDocument();
+    // Ensure button renders
+    const navButton = screen.getByText(/skubak.aj/i);
+    expect(navButton).toBeInTheDocument();
+  });
+
+  test('renders content between header & footer', () => {
+    // Ensure app container element renders
+    const appContent = screen.getByTestId('app');
+    expect(appContent).toBeInTheDocument();
+  });
+
+  test('renders footer & its buttons', () => {
+    // Ensure footer parent element renders
+    const footer = screen.getByTestId('footer');
+    expect(footer).toBeInTheDocument();
+    
+    // Ensure at least some buttons render in footer
+    const allButtons = screen.getAllByRole('button');
+    expect(allButtons.length).toBeGreaterThan(0);
+    
+    /* Check for each button
+      (cant use regex since icon tooltip inherits test id + 'icon'
+      so must be specific)
+    */
+    const homeButton = screen.getByTestId('Home');
+    expect(homeButton).toBeInTheDocument();
+    
+    const linkedinButton = screen.getByTestId('LinkedIn');
+    expect(linkedinButton).toBeInTheDocument();
+
+    const githubButton = screen.getByTestId('GitHub');
+    expect(githubButton).toBeInTheDocument();
+  });
+  
 });
