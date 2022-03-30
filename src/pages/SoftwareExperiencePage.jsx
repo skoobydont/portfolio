03aff23664fef90f2637f12992efcc5d6f54e37c
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-03-30 16:22:58 -0400

    software back button

--- src/pages/SoftwareExperiencePage.jsx
+++ src/pages/SoftwareExperiencePage.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router';
 import makeStyles from '@mui/styles/makeStyles';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import BackIcon from '@mui/icons-material/ArrowBack';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,28 +26,18 @@ const SoftwareExperiencePage = () => {
   const history = useHistory();
   /**
    * Handle Go Back to /porfolio
-   * @fires history.push
-   * @returns {null}
+   * @fires history.goBack
    */
-  const handleGoBack = () => {
-    history.push({
-      pathname: '/portfolio',
-      state: {
-        ...history.location.state,
-        software: true,
-      },
-    });
-    return null;
-  };
+  const handleGoBack = () => history.goBack();
   return (
     <div className={classes.root}>
       <div className={classes.header}>
         <Button
           onClick={() => handleGoBack()}
-          variant="contained"
+          variant="outlined"
           className={classes.button}
         >
-          &#60;
+          <BackIcon />
         </Button>
         <Typography>
           Learn More About Software Experience on this page
