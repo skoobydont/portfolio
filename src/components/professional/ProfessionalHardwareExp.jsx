5e2c7dbec1cbd9fef0b938906be07677f624f52f
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-03-31 19:43:29 -0400

    init professional hardware and software component files

---
+++ src/components/professional/ProfessionalHardwareExp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ProfessionalHardwareExp = () => <div data-testid="professionalHardware">Professional Hardware Experience</div>;
+
+export default ProfessionalHardwareExp;
---
+++ src/components/professional/ProfessionalSoftwareExp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ProfessionalSoftwareExp = () => <div data-testid="professionalSoftware">Professional Software Experience</div>;
+
+export default ProfessionalSoftwareExp;
