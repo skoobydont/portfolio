6fdbf8d2b034f4cb46c6b40c964be8b029b36afe
skoob <ajskubak@gmail.com>
2022-03-31 21:05:38 -0400

    init personal exp components

---
+++ src/components/personal/PersonalHardwareExp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PersonalHardwareExp = () => <div data-testid="personalHardware">Personal Hardware Experience</div>;
+
+export default PersonalHardwareExp;
---
+++ src/components/personal/PersonalSoftwareExp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PersonalSoftwareExp = () => <div data-testid="personalSoftware">Personal Software Experience</div>;
+
+export default PersonalSoftwareExp;
