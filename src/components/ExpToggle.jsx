f120af70d7a90a5663f922deed05265c8b7ff0d6
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-04-01 18:11:05 -0400

    updated expToggle. need to figure out failing test

--- src/components/ExpToggle.jsx
+++ src/components/ExpToggle.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// MUI
+import makeStyles from '@mui/styles/makeStyles';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  active: {
+    color: theme.palette.secondary.main,
+  },
+}));
 /**
  * ExpToggle
  * @param {Object} props 
@@ -14,8 +24,27 @@ const ExpToggle = (props) => {
     exp,
     setExp,
   } = props;
+  const classes = useStyles();
+  /**
+   * Handle Exp State Toggle
+   * @param {string} newExp new value to set exp state
+   * @returns {Function}
+   */
+  const handleExpToggle = (newExp) => setExp(newExp);
 
-  return <div data-testid="exptoggle">Exp Toggle</div>;
+  return (
+    <div data-testid="expToggle">
+      <Button
+        onClick={() => handleExpToggle(
+          exp === 'professional' ? 'personal' : 'professional'
+        )}
+      >
+        <Typography className={exp === 'professional' ? classes.active : ''}>Professional</Typography>
+        <Typography>/</Typography>
+        <Typography className={exp === 'personal' ? classes.active : ''}>Personal</Typography>
+      </Button>
+    </div>
+  );
 };
 
 ExpToggle.propTypes = {
