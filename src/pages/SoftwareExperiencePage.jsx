68e5f5c4954cddbcaa1c28ff693f6018351a8944
skoob <ajskubak@gmail.com>
2022-03-31 21:58:48 -0400

    added init toggle and state var for exp. might want to reduce labels and maybe just have the toggle in the corner while page reacts

--- src/pages/SoftwareExperiencePage.jsx
+++ src/pages/SoftwareExperiencePage.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+// MUI
 import makeStyles from '@mui/styles/makeStyles';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+// Icons
 import BackIcon from '@mui/icons-material/ArrowBack';
-
+// Custom
+import PersonalSoftwareExp from '../components/personal/PersonalSoftwareExp';
+import ProfessionalSoftwareExp from '../components/professional/ProfessionalSoftwareExp';
+// Declare Styles
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(1),
@@ -24,11 +34,22 @@ const useStyles = makeStyles((theme) => ({
 const SoftwareExperiencePage = () => {
   const classes = useStyles();
   const history = useHistory();
+  // Experience State
+  const [exp, setExp] = useState(true);
   /**
    * Handle Go Back to /porfolio
    * @fires history.goBack
    */
   const handleGoBack = () => history.goBack();
+  /**
+   * Handle Experience Toggle
+   * @param {Object} event standard event obj
+   * @fires setExp with event.target.checked
+   */
+  const handleExpToggle = (event) => {
+    setExp(event.target.checked);
+  };
+
   return (
     <div className={classes.root}>
       <div className={classes.header}>
@@ -42,6 +63,22 @@ const SoftwareExperiencePage = () => {
         <Typography>
           Learn More About Software Experience on this page
         </Typography>
+        <FormControl component="fieldset" variant="standard">
+          <FormLabel component="legend">
+            {`${exp ? 'Professional' : 'Personal'} Experience`}
+          </FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={exp}
+                  onChange={handleExpToggle}
+                />
+              }
+              label="Software Experience"
+            />
+          </FormGroup>
+        </FormControl>
       </div>
     </div>
   );
