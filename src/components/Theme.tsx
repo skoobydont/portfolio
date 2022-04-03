3a7fffb96b7ab7d62b122ef6afddad5b17627343
skoob <ajskubak@gmail.com>
2022-04-03 13:30:55 -0400

    added theme interface extension so makeStyles can use palette + spacing

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -3,10 +3,17 @@ import {
   createTheme,
   ThemeProvider,
   StyledEngineProvider,
+  Theme,
 } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from "@mui/material/CssBaseline";
-
+/* Make theme.spacing/palette available to default theme
+  https://stackoverflow.com/questions/70697666/material-ui-v5-property-spacing-does-not-exist-on-type-defaulttheme-when-u
+*/
+declare module '@mui/styles/defaultTheme' {
+  interface DefaultTheme extends Theme {}
+}
+// Theme Props interface
 interface ThemeProps {
   children: boolean |
     React.ReactChild |
