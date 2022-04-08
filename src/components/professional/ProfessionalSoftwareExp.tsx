92ca7f4fadead7c7063a0071d5815eed5c5ff8ba
skoob <ajskubak@gmail.com>
2022-04-08 15:24:21 -0400

    got some content up. going to make exp row component

--- src/components/professional/ProfessionalSoftwareExp.tsx
+++ src/components/professional/ProfessionalSoftwareExp.tsx
@@ -4,13 +4,38 @@ import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import IconButton from '@mui/material/IconButton';
 // Declare Styles
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
+    padding: theme.spacing(1),
+  },
+  expRow: {
+    backgroundColor: theme.palette.primary.light,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  header: {
+    backgroundColor: theme.palette.primary.dark,
+    display: 'flex',
+    flexDirection: 'row',
+    padding: `${theme.spacing(1)} 0 0 ${theme.spacing(1)}`,
   },
 }));
+
+interface ExpRowProps {
+  title: string;
+  children: any;
+  icons: Array<IconArrayItem>,
+};
+
+interface IconArrayItem {
+  altTxt: string;
+  imgSrc: string;
+};
+
 
 const ProfessionalSoftwareExp = () => {
   /*/ Important details to cover:
@@ -20,14 +45,60 @@ const ProfessionalSoftwareExp = () => {
 
   */
   const classes = useStyles();
+  // Exp Row
+  const ExpRow = (props: ExpRowProps) => {
+    const { title, children, icons } = props;
+    return (
+      <Box component={Card} className={classes.expRow}>
+        <Box className={classes.header}>
+          <Typography variant="h6">{title}</Typography>
+        </Box>
+        <Box>
+          {icons.map((icon: IconArrayItem, i: number) => (
+            <IconButton>
+              <img src={icon.imgSrc} alt={icon.altTxt} />
+            </IconButton>
+          ))}
+          {/** icon row? */}
+        </Box>
+        {children}
+      </Box>
+    )
+  }
+
   return (
     <div
       data-testid="professionalSoftware"
       className={classes.root}
+      id="professionalSoftware"
     >
-      <Typography>
+      <Typography variant="h6">
         Professional Software Experience        
       </Typography>
+      <ExpRow title="Developer Co-Op">
+        <Typography>Worked with a linen manufacturer to support digital marketing assets and websites.</Typography>
+        <Typography>HTML5, CSS, JavaScript</Typography>
+      </ExpRow>
+      <ExpRow title="Front End Developer">
+        <Typography>Assisted small digital marketing company with web site management and optimizations for 15+ clients.</Typography>
+        <Typography>PHP, WordPress, HTML, CSS, JavaScript</Typography>
+      </ExpRow>
+      <ExpRow title="Software Engineer">
+        <Typography>Worked as Full Stack developer for clients.</Typography>
+        <Typography>Java, SpringBoot, JUnit, Angular, TypeScript</Typography>
+      </ExpRow>
+      <ExpRow title="Freelance Fullstack Developer">
+        <Typography>Collaborated with restaurant owners to build out a content management system.</Typography>
+        <Typography>PHP, WordPress, HTML, CSS, JavaScript</Typography>
+      </ExpRow>
+      <ExpRow title="Application Developer">
+        <Typography>Joined a small team of developers to maintain and enhance legacy systems while also delivering new applications.</Typography>
+        <Typography>PHP, Laravel, TypeScript, Material UI, JavaScript, React, Redux, Express, Jest, Docker</Typography>
+      </ExpRow>
+      <ExpRow title="Chief Information Technology Officer">
+        <Typography>Starting as the only developer, I now also collaborate with business partners on all IT decisions.</Typography>
+        <Typography>Algorand, MyAlgoConnect, React, JavaScript, Selenium</Typography>
+      </ExpRow>
     </div>
   )
 };
