89d00877fe35ff3ded96f640347a007fe4949869
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-04-15 15:04:51 -0400

    updated env example + env type declaration to handle the tech urls

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,29 @@ declare global {
       REACT_APP_HOME_URL: '/portfolio';
       REACT_APP_LINKED_IN_URL: string;
       REACT_APP_GITHUB_URL: string;
+      // Tech Urls
+      REACT_APP_HTML_URL: string;
+      REACT_APP_CSS_URL: string;
+      REACT_APP_JAVASCRIPT_URL: string;
+      REACT_APP_PHP_URL: string;
+      REACT_APP_WORDPRESS_URL: string;
+      REACT_APP_JAVA_URL: string;
+      REACT_APP_SPRINGBOOT_URL: string;
+      REACT_APP_JUNIT_URL: string;
+      REACT_APP_ANGULAR_URL: string;
+      REACT_APP_TYPESCRIPT_URL: string;
+      REACT_APP_LARAVEL_URL: string;
+      REACT_APP_MATERIALUI_URL: string;
+      REACT_APP_REACT_URL: string;
+      REACT_APP_REDUX_URL: string;
+      REACT_APP_EXPRESS_URL: string;
+      REACT_APP_JEST_URL: string;
+      REACT_APP_DOCKER_URL: string;
+      REACT_APP_ALGORAND_URL: string;
+      REACT_APP_MYALGOCONNECT_URL: string;
+      REACT_APP_SELENIUM_URL: string;
+      REACT_APP_CSHARP_URL: string;
+      REACT_APP_BOOTSTRAP_URL: string;
     }
   }
 }
