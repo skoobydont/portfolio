1d420e01c8d11d29d55c6350ae87ad3654f598c2
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-05-02 12:24:06 -0400

    added starter pics + some styling

--- src/components/personal/PersonalHardwareExp.tsx
+++ src/components/personal/PersonalHardwareExp.tsx
@@ -15,7 +15,6 @@ import CharmanderPrint from '../../img/charmanderPrint.JPG';
 import SquirtlePrint from '../../img/squirtlePrint.JPG';
 import EeveePrint from '../../img/eeveePrint.JPG';
 import PiCluster from '../../img/piCluster.JPG';
-// TODO: pi cluster pics & pokemon starters pic
 // Declare styles
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,16 +22,27 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     padding: theme.spacing(1),
   },
-  img: {
+  medImg: {
     maxHeight: '250px',
     maxWidth: '250px',
   },
   imgRow: {
     display: 'flex',
     flexDirection: 'row',
-    padding: theme.spacing(1),
     justifyContent: 'space-around',
+    alignItems: 'center',
   },
+  imgCol: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& img': {
+      paddingRight: theme.spacing(1),
+    },
+  },
+  smallImg: {
+    maxHeight: '150px',
+    maxWidth: '150px',    
+  }
 }));
 
 /**
@@ -40,13 +50,25 @@ const useStyles = makeStyles((theme) => ({
  * @param props string & src
  * @returns {anchor} with image within
  */
-const NewTabImg = (props: { src: string, text: string }) => {
-  const { src, text } = props;
+const NewTabImg = (props: { src: string, text: string, imgSize: string }) => {
+  const { src, text, imgSize } = props;
   // Declare classes use here instead of passing via props
   const classes = useStyles();
+  // Only supporting small and medium picture sizes for now
+  const imgClass = (size: string) => {
+    switch(size) {
+      case 'small': return classes.smallImg;
+      case 'medium': return classes.medImg;
+      default: return '';
+    }
+  };
   return (
     <a target="_blank" href={src}>
-      <img src={src} alt={text} className={classes.img} />
+      <img
+        src={src}
+        alt={text}
+        className={imgClass(imgSize)}
+      />
     </a>
   );
 }
@@ -68,6 +90,7 @@ const PersonalHardwareExp = () => {
           <NewTabImg
             src={ParentPC}
             text="parental pc"
+            imgSize="medium"
           />
         </Box>
         <Typography>I have since built myself a personal computer as well as assisted friends with their upgrades.</Typography>
@@ -85,18 +108,46 @@ const PersonalHardwareExp = () => {
           <NewTabImg
             src={OctopusPrint}
             text="octopus print"
+            imgSize="medium"
           />
+          <Box className={classes.imgCol}>
+            <Box className={classes.imgRow}>
+              <NewTabImg
+                src={EeveePrint}
+                text="eevee print"
+                imgSize="small"
+              />
+              <NewTabImg
+                src={BulbasaurPrint}
+                text="bulbasaur print"
+                imgSize="small"
+              />
+            </Box>
+            <Box className={classes.imgRow}>
+              <NewTabImg
+                src={CharmanderPrint}
+                text="charmander print"
+                imgSize="small"
+              />
+              <NewTabImg
+                src={SquirtlePrint}
+                text="squirtle print"
+                imgSize="small"
+              />              
+            </Box>
+          </Box>
           <NewTabImg
             src={VasePrint}
             text="vase print"
+            imgSize="medium"
           />
-          {/* <img src={StartersPrint} alt="starters prints" className={classes.img} /> */}
         </Box>
         <Typography>I have since replaced the extruder &amp; plan to upgrade the printer with a new Z axis sensor + configuring OctoPrint.</Typography>
         <Box className={classes.imgRow}>
           <NewTabImg
             src={PrinterUpgrade}
             text="printer upgrades"
+            imgSize="medium"
           />
         </Box>
       </ExpRow>
@@ -109,10 +160,13 @@ const PersonalHardwareExp = () => {
       >
         {/* Pi Cluster & K3S Configuration */}
         <Typography>I may have gone overboard when I installed PoE hats on each pi &amp; supported the build with a 5 port PoE switch and hand-crafted cat5e cables.</Typography>
-        <NewTabImg
-          src={PiCluster}
-          text="raspberry pi cluster"
-        />
+        <Box className={classes.imgRow}>
+          <NewTabImg
+            src={PiCluster}
+            text="raspberry pi cluster"
+            imgSize="medium"
+          />
+        </Box>
         <Typography>I became much more familiar with the terminal and Linux environment while installing necessary packages for k3s &amp; configuring SSH communication.</Typography>
         <Typography variant="caption"><a href="https://www.ansible.com/overview/how-ansible-works?hsLang=en-us" target="_blank">Ansible</a> is another fascinating tool I would love to spend time learning more.</Typography>
       </ExpRow>
